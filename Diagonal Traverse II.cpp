693ee8b24a3661b1a63f734c6d4058bcc7f1a30d
Poornima Srivastava <152088344+Poornima786@users.noreply.github.com>
2023-11-27 22:59:37 +0530

    Create Diagonal Traverse II.cpp

---
+++ Diagonal Traverse II.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> findDiagonalOrder(vector<vector<int>>& nums) {
+    int n = nums.size();
+        // pos vector tells me the elements at the ith diagonal
+        vector<vector<int>> pos(1e5 + 10);
+        vector<int> ans;
+        
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < nums[i].size(); j++){
+                pos[i + j].push_back(nums[i][j]);
+            }
+        }
+        
+        for(int i = 0; i < 1e5 + 10; i++){
+            while(pos[i].size()){
+                ans.push_back(pos[i].back());
+                pos[i].pop_back();  
+            }
+        }
+        
+        return ans;    
+    }
+};
