4fb36f9657791745d6dde4bc1dcab5b9d585cac5
Poornima Srivastava <152088344+Poornima786@users.noreply.github.com>
2024-01-15 12:05:51 +0530

    Create Find Players With Zero or One Losses.cpp

---
+++ Find Players With Zero or One Losses.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<vector<int>> findWinners(vector<vector<int>>& matches) {
+         vector<int> part(1e5 + 10, 0), lose(1e5 + 10, 0);
+
+        for(auto i : matches){
+            part[i[0]] = part[i[1]] = 1;
+
+            ++lose[i[1]];
+        }
+
+        vector<vector<int>> ans(2);
+
+        for(int i = 1; i < 1e5 + 10; i++){
+            if(part[i]){
+                if(lose[i] == 1)
+                    ans[1].push_back(i);
+                else if(lose[i] == 0)
+                    ans[0].push_back(i);
+            }
+        }
+
+        return ans;
+    }
+};
