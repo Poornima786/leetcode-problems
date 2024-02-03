af3d7b57627a537ced3d75a337f3205d38334507
Poornima Srivastava <152088344+Poornima786@users.noreply.github.com>
2024-02-03 12:32:21 +0530

    Create Partition Array for Maximum Sum .cpp

---
+++ Partition Array for Maximum Sum .cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int maxSumAfterPartitioning(vector<int>& arr, int k) {
+        int n = arr.size();
+
+        vector<long long> dp(n + 1, 0);
+        
+        for(int i = n - 1; i > -1; i--){
+            long long MAX = 0;
+
+            for(int j = i; j < min(i + k, n); j++){
+                MAX = max(MAX, (long long)arr[j]);
+
+                dp[i] = max(dp[i], (j - i + 1) * MAX + dp[j + 1]);
+            }
+        }
+
+        return dp[0];
+    }
+};
