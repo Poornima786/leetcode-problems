e731eb71275ae6f86c4e46580aebc174665fb90a
Poornima Srivastava <152088344+Poornima786@users.noreply.github.com>
2024-02-04 13:36:10 +0530

    Create Minimum Window Substring.cpp

---
+++ Minimum Window Substring.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    string minWindow(string s, string t) {
+        int best = 1e9, start = 0;
+        int l = 0, r = 0;
+
+        unordered_map<char,int> fs, ft;
+
+        for(auto i : t)
+            ++ft[i];
+
+        while(r < s.size()){
+            ++fs[s[r++]];
+
+            while(1){
+                bool ok = 1;
+
+                for(auto x : ft){
+                    if(fs[x.first] < x.second){
+                        ok = 0;
+                        break;
+                    }
+                }
+
+                if(!ok)
+                    break;
+
+                if(r - l < best){
+                    best = r - l;
+                    start = l;
+                }
+
+                --fs[s[l++]];
+            }
+        }
+
+        if(best == 1e9)
+            return "";
+        else
+            return s.substr(start, best);
+    }
+};
