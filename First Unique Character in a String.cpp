a66bb67991ebaa9e92b217028097c2f3f275a31a
Poornima Srivastava <152088344+Poornima786@users.noreply.github.com>
2024-02-05 13:49:18 +0530

    Create First Unique Character in a String.cpp

---
+++ First Unique Character in a String.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        vector<int> f(26, 0);
+
+        for(auto i : s)
+            ++f[i - 'a'];
+
+        for(int i = 0; i < s.size(); i++){
+            if(f[s[i] - 'a'] == 1)
+                return i;
+        }
+
+        return -1;
+    }
+};
